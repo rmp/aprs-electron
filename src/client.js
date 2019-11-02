5344e4a659e0e1ae5309ec584d1e0eec461932c2
Roger Pettett <rmp@psyphi.net>
2019-11-02 17:20:28 +0000

    munged in some icons from a random APRS spec

--- src/client.js
+++ src/client.js
@@ -4,81 +4,84 @@ const mapboxgl = require('mapbox-gl/dist/mapbox-gl');
 mapboxgl.accessToken = 'pk.eyJ1Ijoicm1wIiwiYSI6ImNrMjgycGVsbzF0NzczZG1xbXIxdzhja2kifQ.x7x-0X7zQYIfJ7oJlRWdag';
 
 const map = new mapboxgl.Map({
-  container: "mapbox",
-  style: "mapbox://styles/mapbox/streets-v11"
+	container: "mapbox",
+	style: "mapbox://styles/mapbox/streets-v11"
 });
 
 const data = {
     "type": "FeatureCollection",
-    "features": [
-    ]
+    "features": []
 };
 
 map.on('load', () => {
 
     map.addSource('aprssource', { type: 'geojson', data });
 
-    map.loadImage('https://upload.wikimedia.org/wikipedia/commons/thumb/6/60/Cat_silhouette.svg/400px-Cat_silhouette.svg.png', (error, image) => {
-		if (error) throw error;
-		//map.addImage('cat', image);
-		map.addLayer({
-			"id": "aprslayer",
-			"type": "symbol",
-			"source": 'aprssource',
-			"layout": {
-				"icon-image": "rocket-15"
-			},
-//			"filter": ["==", "$type", "Point"],
+	const imagePromises = [];
+	for (let i=33; i<=128; i++) {
+		imagePromises.push(new Promise((resolve, reject) => {
+			map.loadImage(`gfx/${i}.png`, (error, image) => {
+				if(error) { reject(error); return; }
+				map.addImage(i, image);
+				resolve();
+			});
+		}));
+	}
 
-			/*			"layout": {
-				"icon-image": "cat",
-				"icon-size": 0.1
-			}*/
+	Promise
+		.all(imagePromises)
+		.then(() => {
+			map.addLayer({
+				"id": "aprslayer",
+				"type": "symbol",
+				"source": 'aprssource',
+				"layout": {
+					"icon-image": ['get', 'symbol'], //"rocket-15"
+				},
+			})
+		})
+		.catch(error => {
+			throw error;
 		});
-	});
-
-	ipcRenderer.on('aprs', (event, obj) => {
-
-		if(!obj.data || !obj.data.latitude || !obj.data.longitude) {
-			return;
-		}
-
-		const { latitude, longitude } = obj.data;
-		console.log(obj);
-		data.features.unshift({
-			id: `${obj.from.call}_${obj.from.ssid}`,
-			type: 'Feature',
-			geometry: {
-				type: 'Point',
-				coordinates: [longitude, latitude],
-				properties: {
-					description: `${obj.from.call}`,
-				}
-			}
-		});
-		map.getSource('aprssource').setData(data);	
-		/*	data {
-			latitude: 33.642
-			longitude: -96.6305
-			msgEnabled: false
-			symbol: "/#"
-			symbolIcon: "Digi"
-			}
-		*/
-	});
 });
 
-var popup = new mapboxgl.Popup({
+ipcRenderer.on('aprs', (event, obj) => {
+
+	if(!obj.data || !obj.data.latitude || !obj.data.longitude) {
+		return;
+	}
+
+	const { latitude, longitude } = obj.data;
+//		console.log(obj);
+	const chr = obj.data.symbol.charCodeAt(1); // two-char symbol e.g. "/_" or "/#"
+
+	data.features.unshift({
+		id: `${obj.from.call}_${obj.from.ssid}`,
+		type: 'Feature',
+		geometry: {
+			type: 'Point',
+			coordinates: [longitude, latitude],
+		},
+		properties: {
+			description: `${obj.from.call}`,
+			symbol: chr,
+		}
+	});
+
+	map.getSource('aprssource').setData(data);	
+});
+
+const popup = new mapboxgl.Popup({
 	closeButton: false,
-	closeOnClick: false
+	closeOnClick: false,
 });
 
 map.on('mouseenter', 'aprslayer', function(e) {
 	// Change the cursor style as a UI indicator.
 	map.getCanvas().style.cursor = 'pointer';
 
-	var coordinates = e.features[0].geometry.coordinates.slice();
-	var description = e.features[0].properties.description;
+	const coordinates = e.features[0].geometry.coordinates.slice();
+	const description = e.features[0].properties.description;
 
 	// Ensure that if the map is zoomed out such that multiple
 	// copies of the feature are visible, the popup appears
